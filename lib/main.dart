4470d29b22608acd3b51171505254a2d6302a062
Devang <devangdhameliya5@gmail.com>
2026-01-02 11:55:23 +0530

    Refactor: Update main to use HomePage2 and hide debug banner
    - Removed the import for the obsolete `home_page.dart`.
    - Set `HomePage2` as the new home widget.
    - Disabled the debug mode banner in the `MaterialApp`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebaseeasycrud/firebase_options.dart';
-import 'package:firebaseeasycrud/home_page.dart';
 import 'package:firebaseeasycrud/home_page2.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage2(),
